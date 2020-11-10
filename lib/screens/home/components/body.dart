65b9e6bb3b2ac7d28179875af2cd3d12fac486d7
Thong Nguyen <thong.nguyen@hdwebsoft.co>
2020-11-10 16:58:42 +0700

    add banner shop

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:shop_app/constants.dart';
 import 'package:shop_app/size_config.dart';
+
+import 'discount_banner.dart';
+import 'home_header.dart';
 
 class Body extends StatelessWidget {
   const Body({Key key}) : super(key: key);
@@ -13,98 +14,11 @@ class Body extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(height: getProportionateScreenHeight(20)),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: getProportionateScreenWidth(20)),
-              child: Row(
-                children: [
-                  Container(
-                    width: SizeConfig.screenWidth * 0.6,
-                    //height: 50,
-                    decoration: BoxDecoration(
-                      color: kSecondaryColor.withOpacity(0.1),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: TextField(
-                      onChanged: (value) {
-                        //Search value
-                      },
-                      decoration: InputDecoration(
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        hintText: "Search Product",
-                        prefixIcon: const Icon(Icons.search),
-                        contentPadding: EdgeInsets.symmetric(
-                          horizontal: getProportionateScreenWidth(20),
-                          vertical: getProportionateScreenWidth(9),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const IconBtnWithCounter(),
-                ],
-              ),
-            )
+            const HomeHeader(),
+            SizedBox(height: getProportionateScreenHeight(20)),
+            const DiscountBanner(),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class IconBtnWithCounter extends StatelessWidget {
-  const IconBtnWithCounter({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {},
-      borderRadius: BorderRadius.circular(50),
-      child: Stack(
-        overflow: Overflow.visible,
-        children: [
-          Container(
-            padding: EdgeInsets.all(
-              getProportionateScreenWidth(12),
-            ),
-            height: getProportionateScreenHeight(46),
-            width: getProportionateScreenWidth(46),
-            decoration: BoxDecoration(
-              color: kSecondaryColor.withOpacity(0.1),
-              shape: BoxShape.circle,
-            ),
-            child: SvgPicture.asset("assets/icons/Bell.svg"),
-          ),
-          Positioned(
-            top: -3,
-            right: 0,
-            child: Container(
-              width: getProportionateScreenWidth(16),
-              height: getProportionateScreenWidth(16),
-              decoration: BoxDecoration(
-                color: const Color(0xFFFF4848),
-                shape: BoxShape.circle,
-                border: Border.all(
-                  width: 1.5,
-                  color: Colors.white,
-                ),
-              ),
-              child: Center(
-                child: Text(
-                  "3",
-                  style: TextStyle(
-                    fontSize: getProportionateScreenWidth(10),
-                    height: 1,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-            ),
-          )
-        ],
       ),
     );
   }
---
+++ lib/screens/home/components/home_header.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../../../size_config.dart';
+import 'icon_btn_with_counter.dart';
+import 'search_field.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const SearchField(),
+          IconBtnWithCounter(
+            press: () {},
+            svgSrc: "assets/icons/Cart Icon.svg",
+          ),
+          IconBtnWithCounter(
+            press: () {},
+            svgSrc: "assets/icons/Bell.svg",
+            numOfItems: 3,
+          ),
+        ],
+      ),
+    );
+  }
+}
