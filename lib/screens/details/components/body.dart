c39c49d265fcda1595e1c281a4fb45a87be37a46
Thong Nguyen <thong.nguyen@hdwebsoft.co>
2020-11-29 09:35:11 +0700

    Add product description

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:shop_app/models/Product.dart';
-import 'package:shop_app/size_config.dart';
 
+import 'product_description.dart';
 import 'product_images.dart';
+import 'top_rounded_container.dart';
 
 class Body extends StatelessWidget {
   final Product product;
@@ -17,72 +17,12 @@ class Body extends StatelessWidget {
         ProductImages(product: product),
         TopRoundedContainer(
           color: Colors.white,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: getProportionateScreenWidth(20)),
-                child: Text(
-                  product.title,
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ),
-              const SizedBox(height: 5),
-              Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  padding: EdgeInsets.all(getProportionateScreenWidth(15)),
-                  width: getProportionateScreenWidth(64),
-                  decoration: BoxDecoration(
-                    color: product.isFavourite
-                        ? const Color(0xFFFFE6E6)
-                        : const Color(0xFFF5F6F9),
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      bottomLeft: Radius.circular(20),
-                    ),
-                  ),
-                  child: SvgPicture.asset(
-                    "assets/icons/Heart Icon_2.svg",
-                    color: product.isFavourite
-                        ? const Color(0xFFFF4848)
-                        : const Color(0xFFDBDEE4),
-                  ),
-                ),
-              )
-            ],
+          child: ProductionDescription(
+            product: product,
+            pressOnSeeMore: () {},
           ),
         ),
       ],
-    );
-  }
-}
-
-class TopRoundedContainer extends StatelessWidget {
-  const TopRoundedContainer({
-    @required this.color,
-    @required this.child,
-    Key key,
-  }) : super(key: key);
-
-  final Color color;
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: getProportionateScreenWidth(20)),
-      padding: EdgeInsets.only(top: getProportionateScreenWidth(20)),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(40),
-          topRight: Radius.circular(40),
-        ),
-      ),
-      child: child,
     );
   }
 }
